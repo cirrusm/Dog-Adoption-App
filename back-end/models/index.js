1b981e045818a0b024f8f4a44479ed9b361e2599
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-11-23 14:00:04 -0800

    initialized starting folders and started on models

---
+++ back-end/models/index.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+const connectionString = "mongodb://localhost:27017/dogs"
+
+const configOptions = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+};
+
+mongoose
+  .connect(connectionString, configOptions)
+  .then(() => console.log("MongoDB successfully connected..."))
+  .catch((err) => console.log(`MongoDB connection error: ${err}`));
+
+  // module.exports = {
+
+  // }
