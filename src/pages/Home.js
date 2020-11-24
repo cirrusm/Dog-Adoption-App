92e0312448987ddbf12a8faf39b17f8faee737f1
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-11-23 21:13:30 -0800

    api call working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,61 @@ import {Link} from 'react-router-dom'
 
 class Home extends Component {
   state = {  }
+
+  componentDidMount() {
+    let key = 'rqgLcLaO8ZSjNJDMdqy1i19a3qZBrDVBrcGHHWF366C34RtFJN'
+let secret = 'kfQMxVox4kH2I7A56kwo1JPrJyoRuQEnxWHwfoDc'
+let org ="RI77"
+let status= "adoptable"
+let size="medium"
+let location = 92625
+let goodWithDogs = "true"
+let goodWithChildren = "false"
+let age = 'baby'
+let coat = ''
+
+fetch('https://api.petfinder.com/v2/oauth2/token', {
+	method: 'POST',
+	body: 'grant_type=client_credentials&client_id=' + key + '&client_secret=' + secret,
+	headers: {
+		'Content-Type': 'application/x-www-form-urlencoded'
+	}
+}).then(function (resp) {
+
+	// Return the response as JSON
+	return resp.json();
+
+}).then(function (data) {
+
+	// Log the API data
+	console.log('token', data);
+
+	// Return a second API call
+	// This one uses the token we received for authentication
+	return fetch('https://api.petfinder.com/v2/animals?limit=5&type=dog&location=' + location + '&good_with_dogs=' + goodWithDogs + '&size=' + size + '&good_with_children=' + goodWithChildren + '&age=' + age + '&coat=' + coat,  {
+		headers: {
+			'Authorization': data.token_type + ' ' + data.access_token,
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
+	});
+
+}).then(function (resp) {
+
+	// Return the API response as JSON
+	return resp.json();
+
+}).then(function (data) {
+
+	// Log the pet data
+	console.log('pets', data);
+
+}).catch(function (err) {
+
+	// Log any errors
+	console.log('something went wrong', err);
+
+});
+  }
   render() { 
     return ( 
       <div>
