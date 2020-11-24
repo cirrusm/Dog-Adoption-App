23065894279d33ef9ae08c357f90ab688ecf9800
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-11-24 11:44:40 -0800

    form updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = (props) => {
     <div>
         <nav className="main-nav">
           <div class="nav-wrapper">
-            <div class="brand-logo">Puppy Love</div>
+            <div class="brand-logo"><Link to ='/'>Puppy Love</Link></div>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
               <li><Link to="/">Home</Link></li>
               <li><Link to="/login">Login</Link></li>
